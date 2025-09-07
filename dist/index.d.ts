2b18958ce717cadf426a8f7324c45451f529a1bb
sbonelosth <31245826+sbonelosth@users.noreply.github.com>
2025-09-07 04:58:36 +0200

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { defineMernAccessConfig } from "./config";
-export type { MernAccessClientConfig } from "./config";
-export { MernAccessProvider, useMernAccess } from "./context/MernAccessContext";
-export { mernAccessService } from "./services/mernAccessService";
-export type * from "./types";
