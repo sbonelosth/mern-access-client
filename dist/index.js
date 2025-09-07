4f3fc5cae232729dfb0a6380cc18d2e0e03f82aa
sbonelosth <31245826+sbonelosth@users.noreply.github.com>
2025-09-07 04:59:13 +0200

    add .gitignore

--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export { defineMernAccessConfig } from "./config";
-export { MernAccessProvider, useMernAccess } from "./context/MernAccessContext";
-export { mernAccessService } from "./services/mernAccessService";
